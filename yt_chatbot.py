33d6f12dbbf9919d126b381db4d7c5f95d064f6c
Abhishek Purohit <apurohit1306@gmail.com>
2026-06-01 10:52:27 +0530

    Extracted vector store creation into separate module

--- yt_chatbot.py
+++ yt_chatbot.py
@@ -1,8 +1,6 @@
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain_core.prompts import ChatPromptTemplate
@@ -10,34 +8,18 @@ from langchain_core.prompts import MessagesPlaceholder
 from langchain_core.messages import HumanMessage
 from langchain_core.messages import AIMessage
 from app.ingestion import get_video_chunks
+from app.vectorstore import create_vectorstore
 from dotenv import load_dotenv
 load_dotenv()
 
 
 
 video_id = "oAkLSJNr5zY" 
-# try:
-#     ytt_api = YouTubeTranscriptApi()
-#     transcript_list = ytt_api.fetch(video_id, languages=['en'])
-
-#     transcript = " ".join(chunk.text for chunk in transcript_list)
-    
-
-# except TranscriptsDisabled:
-#     print("Transcripts are disabled for this video.")
-
-
-# splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-# chunks = splitter.create_documents([transcript])
-
 chunks = get_video_chunks(video_id)
 
 
 
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-vector_store = FAISS.from_documents(chunks, embeddings)
+vector_store = create_vectorstore(chunks)
 
 retriever = MultiQueryRetriever.from_llm(
     retriever = vector_store.as_retriever(search_type="similarity",search_kwargs={"k": 4}),
