f761a639a132cd2934779b7e6782fe6ca10ce0eb
Abhishek Purohit <apurohit1306@gmail.com>
2026-06-01 12:04:44 +0530

    Refactored chatbot logic into class-based architecture

---
+++ app/chatbot.py
@@ -0,0 +1,75 @@
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import HumanMessage
+from langchain_core.messages import AIMessage
+from app.prompts import get_youtube_prompt
+
+class YouTubeChatbot:
+
+    def __init__(self, retriever):
+        self.retriever = retriever
+        self.llm = ChatGoogleGenerativeAI(
+            model="models/gemini-2.5-flash-lite",
+            temperature=0.2
+        )
+        self.prompt = get_youtube_prompt()
+        self.chat_history = []
+        self.parser = StrOutputParser()
+        self.parallel_chain = RunnableParallel({
+            "context": retriever | RunnableLambda(self.format_doc),
+            "question": RunnablePassthrough(),
+            "chat_history": RunnableLambda(
+                lambda _: self.chat_history
+            )
+        })
+        self.main_chain = (self.parallel_chain | self.prompt | self.llm | self.parser)
+
+
+
+    def format_doc(self,  retrieved_docs):
+        return "\n\n".join(
+            doc.page_content
+            for doc in retrieved_docs
+        )
+    
+
+    
+    def ask(self, question):
+
+        answer = self.main_chain.invoke(
+            question
+        )
+
+        self.chat_history.append(
+            HumanMessage(content=question)
+        )
+
+        self.chat_history.append(
+            AIMessage(content=answer)
+        )
+
+        return answer
+    
+
+
+    def chat(self):
+
+        while True:
+
+            question = input(
+                "\nYou: "
+            )
+
+            if question.lower() == "exit":
+                break
+
+            answer = self.ask(
+                question
+            )
+
+            print("\nAI:")
+            print(answer)
+
+        
--- yt_chatbot.py
+++ yt_chatbot.py
@@ -1,13 +1,10 @@
-from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
-from langchain_core.output_parsers import StrOutputParser
+
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
-from langchain_core.prompts import PromptTemplate
-from langchain_core.messages import HumanMessage
-from langchain_core.messages import AIMessage
 from app.ingestion import get_video_chunks
 from app.vectorstore import create_vectorstore
 from app.retriever import create_retriever
 from app.prompts import get_youtube_prompt
+from app.chatbot import YouTubeChatbot
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -29,46 +26,50 @@ llm = ChatGoogleGenerativeAI(model="models/gemini-2.5-flash-lite", temperature=0
 
 
 
-def format_doc(retrieved_docs):
-    context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
-    return context_text
+# def format_doc(retrieved_docs):
+#     context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+#     return context_text
 
 
-chat_history = []
+# chat_history = []
 
 
-parser = StrOutputParser()
+# parser = StrOutputParser()
 
-parallel_chain = RunnableParallel({
-    "context": retriever | RunnableLambda(format_doc),
-    "question": RunnablePassthrough(),
-    "chat_history": RunnableLambda(
-        lambda _: chat_history
-    )
-})
+# parallel_chain = RunnableParallel({
+#     "context": retriever | RunnableLambda(format_doc),
+#     "question": RunnablePassthrough(),
+#     "chat_history": RunnableLambda(
+#         lambda _: chat_history
+#     )
+# })
 
-main_chain = parallel_chain | prompt | llm | parser
+# main_chain = parallel_chain | prompt | llm | parser
 
-while True:
+# while True:
 
-    question = input("\nYou: ")
+#     question = input("\nYou: ")
 
-    if question.lower() == "exit":
-        break
+#     if question.lower() == "exit":
+#         break
 
-    answer = main_chain.invoke(question)
+#     answer = main_chain.invoke(question)
 
-    print("\nAI:")
-    print(answer)
+#     print("\nAI:")
+#     print(answer)
 
-    chat_history.append(
-        HumanMessage(content=question)
-    )
+#     chat_history.append(
+#         HumanMessage(content=question)
+#     )
 
-    chat_history.append(
-        AIMessage(content=answer)
-    )
+#     chat_history.append(
+#         AIMessage(content=answer)
+#     )
 
+
+bot = YouTubeChatbot(retriever)
+
+bot.chat()
 
 
 
