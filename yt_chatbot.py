8e4c9e9223ad8f28b3b6b015bf04622c4b2d1b23
Abhishek Purohit <apurohit1306@gmail.com>
2026-06-03 11:20:13 +0530

    Simplified terminal entry point

--- yt_chatbot.py
+++ yt_chatbot.py
@@ -1,4 +1,3 @@
-
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
 from app.ingestion import get_video_chunks
 from app.vectorstore import create_vectorstore
@@ -8,11 +7,8 @@ from app.chatbot import YouTubeChatbot
 from dotenv import load_dotenv
 load_dotenv()
 
-
-
 video_id = "oAkLSJNr5zY" 
 chunks = get_video_chunks(video_id)
-
 
 
 vector_store = create_vectorstore(chunks)
@@ -21,55 +17,7 @@ retriever = create_retriever(vector_store)
 
 prompt = get_youtube_prompt()
 
-llm = ChatGoogleGenerativeAI(model="models/gemini-2.5-flash-lite", temperature=0.2)
-
-
-
-
-# def format_doc(retrieved_docs):
-#     context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
-#     return context_text
-
-
-# chat_history = []
-
-
-# parser = StrOutputParser()
-
-# parallel_chain = RunnableParallel({
-#     "context": retriever | RunnableLambda(format_doc),
-#     "question": RunnablePassthrough(),
-#     "chat_history": RunnableLambda(
-#         lambda _: chat_history
-#     )
-# })
-
-# main_chain = parallel_chain | prompt | llm | parser
-
-# while True:
-
-#     question = input("\nYou: ")
-
-#     if question.lower() == "exit":
-#         break
-
-#     answer = main_chain.invoke(question)
-
-#     print("\nAI:")
-#     print(answer)
-
-#     chat_history.append(
-#         HumanMessage(content=question)
-#     )
-
-#     chat_history.append(
-#         AIMessage(content=answer)
-#     )
-
 
 bot = YouTubeChatbot(retriever)
 
 bot.chat()
-
-
-
