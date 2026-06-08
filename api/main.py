ab81c6904cb518281b8dd2dfaccd89c9ae0c79d2
Abhishek Purohit <apurohit1306@gmail.com>
2026-06-08 09:18:44 +0530

    added fast api endpoint get and post process video

---
+++ api/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.utils import extract_video_id
+from app.ingestion import get_video_chunks
+from app.vectorstore import get_or_create_vectorstore
+from app.retriever import create_retriever
+from app.chatbot import YouTubeChatbot
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class VideoRequest(BaseModel):
+    video_url: str
+
+app = FastAPI()
+chatbot = None
+
+
+
+@app.get("/")
+def home():
+
+    return {
+        "message": "YouTube Chatbot API"
+    }
+
+@app.post("/process-video")
+def process_video(request: VideoRequest):
+
+    global chatbot
+    video_id = extract_video_id(
+        request.video_url
+    )
+    if not video_id:
+        return {
+            "status": "error",
+            "message": "Invalid YouTube URL"
+        }
+
+    vector_store = get_or_create_vectorstore(
+        video_id,
+        lambda: get_video_chunks(video_id)
+    )
+
+    retriever = create_retriever(
+        vector_store
+    )
+
+    chatbot = YouTubeChatbot(
+        retriever
+    )
+
+    return {
+        "status": "success"
+    }
